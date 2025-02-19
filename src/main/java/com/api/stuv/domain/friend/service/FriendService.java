cccfe6475cb7075498103698b9a65f5463002468
Dianuma <dltmdgus1208@gmail.com>
2025-02-19 22:26:37 +0900

    refactor: About the Friend 친구 요청 기능 수정 - FriendRequestResponse -> FriendFollowResponse 변경 - wrapping class 제거하고 개별 클래스로 분리

--- src/main/java/com/api/stuv/domain/friend/service/FriendService.java
+++ src/main/java/com/api/stuv/domain/friend/service/FriendService.java
@@ -1,6 +1,6 @@
 package com.api.stuv.domain.friend.service;
 
-import com.api.stuv.domain.friend.dto.FriendResponse.*;
+import com.api.stuv.domain.friend.dto.*;
 import com.api.stuv.domain.friend.entity.Friend;
 import com.api.stuv.domain.friend.repository.FriendRepository;
 import com.api.stuv.domain.user.repository.UserRepository;
@@ -18,13 +18,13 @@ public class FriendService {
     private final FriendRepository friendRepository;
 
     @Transactional
-    public FriendRequestResponse requestFriend(Long userId, Long friendId) {
+    public FriendFollowResponse requestFriend(Long userId, Long friendId) {
         // 두 유저가 존재하지 않을 경우
         if ( userRepository.isDuplicateIds(Arrays.asList(userId, friendId)) != 2 ) throw new IllegalArgumentException("존재하지 않는 유저입니다."); // 나중에 Error Code 수정
 
         // 이미 친구 요청을 보냈을 경우
         if (friendRepository.isFriendshipDuplicate(userId, friendId) > 0) throw new IllegalArgumentException("이미 친구 요청을 보냈습니다."); // 나중에 Error Code 수정
 
-        return FriendRequestResponse.from(friendRepository.save(Friend.create(userId, friendId)));
+        return FriendFollowResponse.from(friendRepository.save(Friend.create(userId, friendId)));
     }
 }
