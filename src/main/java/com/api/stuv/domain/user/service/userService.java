199ff7cf310df7363dde329defbb4e4ed5188433
Sehyun Jin <niki8533@gmail.com>
2025-02-19 23:57:31 +0900

    feat: 랜덤 닉네임 설정 기능 추가

--- src/main/java/com/api/stuv/domain/user/service/userService.java
+++ src/main/java/com/api/stuv/domain/user/service/userService.java
@@ -4,20 +4,27 @@ import com.api.stuv.domain.user.dto.request.UserRequest;
 import com.api.stuv.domain.user.entity.RoleType;
 import com.api.stuv.domain.user.entity.User;
 import com.api.stuv.domain.user.repository.UserRepository;
+import com.api.stuv.domain.user.response.GenerateNicknameResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Service
 @RequiredArgsConstructor
 public class userService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final WebClient webClient;
+    private static final String NICKNAME_GENERATOR_URI = "https://nickname.hwanmoo.kr/?format=json";
 
     public void registerUser(UserRequest userRequest) {
         String email = userRequest.email();
         String password = userRequest.password();
-        String nickname = userRequest.nickname();
+        String nickname = userRequest.nickname() != null? userRequest.nickname() : randomName();
 
         Boolean isExist = userRepository.existsByEmail(email);
 
@@ -35,5 +42,17 @@ public class userService {
                 .build();
 
         userRepository.save(user);
+    }
+
+    public String randomName(){
+        GenerateNicknameResponse response = webClient.get()
+                .uri(NICKNAME_GENERATOR_URI)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToMono(GenerateNicknameResponse.class)
+                .block();
+
+        String randomNickname = String.join("", response.getWords());
+        return randomNickname;
     }
 }
