181a3345e1748553f67a0498d91d9626a18ea64c
Sehyun Jin <niki8533@gmail.com>
2025-02-20 16:02:31 +0900

    Feature: 이메일 인증 후 회원가입 처리

--- src/main/java/com/api/stuv/domain/user/dto/request/UserRequest.java
+++ src/main/java/com/api/stuv/domain/user/dto/request/UserRequest.java
@@ -3,7 +3,6 @@ package com.api.stuv.domain.user.dto.request;
 public record UserRequest(
         String email,
         String password,
-        String nickname,
-        boolean verified
+        String nickname
 ) {
 }
