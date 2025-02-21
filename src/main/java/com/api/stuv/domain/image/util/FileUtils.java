5448690794cb08ba09890e9115f29a979ac3a081
you <20211939@edu.hanbat.ac.kr>
2025-02-21 11:03:07 +0900

    perf: static final 사용

--- src/main/java/com/api/stuv/domain/image/util/FileUtils.java
+++ src/main/java/com/api/stuv/domain/image/util/FileUtils.java
@@ -1,6 +1,5 @@
 package com.api.stuv.domain.image.util;
 
-import io.netty.util.internal.StringUtil;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,9 +8,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public class FileUtils {
+
+    public static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     public static String generateNewFilename() {
         LocalDateTime now = LocalDateTime.now();
-        String datePart = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String datePart = now.format(dateTimeFormatter);
         String uniquePart = UUID.randomUUID().toString().substring(0, 13).toUpperCase().replace("-", "");
 
         return String.format("%s_%s", datePart, uniquePart);
