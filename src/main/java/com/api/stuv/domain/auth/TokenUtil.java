99cab66c3ea849b9a0df45669caee2097a14d291
Sehyun Jin <niki8533@gmail.com>
2025-02-22 13:55:51 +0900

    feature: 토큰 정보 받아오는 클래스 생성

---
+++ src/main/java/com/api/stuv/domain/auth/TokenUtil.java
@@ -0,0 +1,40 @@
+package com.api.stuv.domain.auth;
+
+import com.api.stuv.domain.auth.dto.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+@Component
+public class TokenUtil {
+
+    public String getEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        return customUserDetails.getUsername();
+    }
+
+    public Long getUserId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        return customUserDetails.getUserId();
+    }
+
+    public String getRole() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+
+        GrantedAuthority grantedAuthority = iter.next();
+
+        return grantedAuthority.getAuthority();
+    }
+}
