2b913fb8f46a674d082845f4051ab290c1b31842
jeong-sys <20211939@edu.hanbat.ac.kr>
2025-02-23 13:18:07 +0900

    perf: 코스튬 목록 조회 쿼리문 수정 - total Count 조회 - 페이징 처리

--- src/main/java/com/api/stuv/domain/shop/repository/CostumeRepositoryImpl.java
+++ src/main/java/com/api/stuv/domain/shop/repository/CostumeRepositoryImpl.java
@@ -3,7 +3,7 @@ package com.api.stuv.domain.shop.repository;
 import com.api.stuv.domain.image.entity.ImageType;
 import com.api.stuv.domain.image.entity.QImageFile;
 import com.api.stuv.domain.image.service.S3ImageService;
-import com.api.stuv.domain.shop.dto.CostumeListResponse;
+import com.api.stuv.domain.shop.dto.CostumeResponse;
 import com.api.stuv.domain.shop.entity.QCostume;
 import com.api.stuv.global.response.PageResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -21,10 +21,10 @@ public class CostumeRepositoryImpl implements CostumeRepositoryCustom {
     private final S3ImageService s3ImageService;
 
     @Override
-    public PageResponse<CostumeListResponse> getCostumeList(Pageable pageable) {
+    public PageResponse<CostumeResponse> getCostumeList(Pageable pageable) {
 
-        long totalCount = jpaQueryFactory.selectFrom(qCostume).fetch().size();
-        List<CostumeListResponse> listResponses = jpaQueryFactory
+        long totalCount = jpaQueryFactory.select(qCostume.count()).from(qCostume).fetchOne();
+        List<CostumeResponse> listResponses = jpaQueryFactory
                 .select(qCostume.id,
                         qImageFile.newFilename,
                         qCostume.costumeName,
@@ -32,9 +32,11 @@ public class CostumeRepositoryImpl implements CostumeRepositoryCustom {
                 .from(qCostume)
                 .leftJoin(qImageFile)
                 .on(qImageFile.id.eq(qCostume.imagefileId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
                 .fetch()
                 .stream()
-                .map(tuple -> new CostumeListResponse(
+                .map(tuple -> new CostumeResponse(
                         tuple.get(qCostume.id),
                         s3ImageService.generateImageFile(ImageType.COSTUME, tuple.get(qCostume.id), tuple.get(qImageFile.newFilename)),
                         tuple.get(qCostume.costumeName),
