62ea29c4fb84d4962bef1ca15204ecc48d358d8c
jeong-sys <20211939@edu.hanbat.ac.kr>
2025-02-23 16:58:07 +0900

    feat: 코스튬 삭제 예외 처리 추가

--- src/main/java/com/api/stuv/global/exception/ErrorCode.java
+++ src/main/java/com/api/stuv/global/exception/ErrorCode.java
@@ -50,6 +50,9 @@ public enum ErrorCode {
     COMMENT_NOT_FOUND(404, -6000, "해당 댓글을 찾을 수 없습니다."),
     COMMENT_ALREADY_CONFIRM(400, -6001, "이미 채택된 댓글이 있습니다."),
     COMMENT_NOT_AUTHORIZED(403, -6002, "해당 댓글에 대한 권한이 없습니다."),
+
+    // IMAGE
+    IMAGE_NAME_NOT_FOUND(404, -8000, "해당 이미지를 찾을 수 없습니다.")
     ;
 
     private final int status;
