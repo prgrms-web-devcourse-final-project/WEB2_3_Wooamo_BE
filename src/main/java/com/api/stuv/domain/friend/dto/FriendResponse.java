93d5ac79c068eda74aa67d57d4007b1fa1c91064
Dianuma <dltmdgus1208@gmail.com>
2025-02-25 02:31:28 +0900

    refactor: About the Friend - Response 및 Request 수정 - 일부 로직 수정

--- src/main/java/com/api/stuv/domain/friend/dto/FriendResponse.java
+++ src/main/java/com/api/stuv/domain/friend/dto/FriendResponse.java
@@ -1,6 +1,10 @@
 package com.api.stuv.domain.friend.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record FriendResponse(
+        Long friendId,
         Long userId,
         String nickname,
         String context,
