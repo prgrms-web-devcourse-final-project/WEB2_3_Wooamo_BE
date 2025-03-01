b58fe33da28bbde20c372cffd4ff80861962100f
Sehyun Jin <niki8533@gmail.com>
2025-03-01 21:04:27 +0900

    fix: cookie 설정 변경

--- src/main/java/com/api/stuv/domain/auth/jwt/CustomLogoutFilter.java
+++ src/main/java/com/api/stuv/domain/auth/jwt/CustomLogoutFilter.java
@@ -95,7 +95,7 @@ public class CustomLogoutFilter extends GenericFilterBean {
         Cookie cookie = new Cookie("refresh", null);
         cookie.setMaxAge(0);
         cookie.setPath("/");
-        cookie.setDomain("localhost");
+        cookie.setDomain("");
         response.addCookie(cookie);
 
         response.setStatus(HttpServletResponse.SC_OK);
--- src/main/java/com/api/stuv/domain/auth/jwt/LoginFilter.java
+++ src/main/java/com/api/stuv/domain/auth/jwt/LoginFilter.java
@@ -126,7 +126,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(24*60*60);
         cookie.setHttpOnly(true);
-        cookie.setDomain("localhost");
+        cookie.setDomain("");
         cookie.setPath("/");
 
         return cookie;
