c477e98528246f53f89d3ee57728cf696c76c528
niki8533 <niki8533@naver.com>
2025-03-03 22:32:31 +0900

    fix: 남의 정보 조회 API에서 friendId 같이 보내주기

--- src/main/java/com/api/stuv/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/api/stuv/domain/user/repository/UserRepositoryImpl.java
@@ -65,7 +65,7 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
     @Override
    public UserInformationResponse getUserInformation(Long userId, Long myId, Long friends) {
        Tuple informationDetails = jpaQueryFactory
-               .select(u.id, u.context, u.blogLink, u.nickname, f.status, u.costumeId, i.newFilename, uc.costumeId)
+               .select(u.id, u.context, u.blogLink, u.nickname, f.status, f.id, u.costumeId, i.newFilename, uc.costumeId)
                .from(u)
                .leftJoin(f).on
                        (f.userId.eq(userId).and(f.friendId.eq(myId))
@@ -95,7 +95,8 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
                        informationDetails.get(i.newFilename)
                ),
                status,
-               friends
+               friends,
+               informationDetails.get(f.id)
        );
     }
 
