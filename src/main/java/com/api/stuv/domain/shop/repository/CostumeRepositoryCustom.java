636d2797b6e6333284023e0ab6f14c87b5f5363b
DevPumpkin0113 <agg0113@gmail.com>
2025-03-04 16:12:45 +0900

    feat: 주간 관리자 정보 조회 기능 구현

--- src/main/java/com/api/stuv/domain/shop/repository/CostumeRepositoryCustom.java
+++ src/main/java/com/api/stuv/domain/shop/repository/CostumeRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.api.stuv.domain.shop.repository;
 
+import com.api.stuv.domain.image.dto.ImageDTO;
 import com.api.stuv.domain.shop.dto.costume.CostumeDTO;
 import com.api.stuv.global.response.PageResponse;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface CostumeRepositoryCustom {
+    ImageDTO findCostumeByBestSales();
     PageResponse<CostumeDTO> getCostumeList(Pageable pageable);
     Optional<CostumeDTO> getCostume(Long id);
 }
