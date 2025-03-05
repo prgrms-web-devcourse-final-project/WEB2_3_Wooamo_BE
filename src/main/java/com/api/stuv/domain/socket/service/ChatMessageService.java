20b2e0476728dae06c07911a50afc302b29f3407
DongYoon <kdu0516@gmail.com>
2025-03-05 14:19:21 +0900

    no message

--- src/main/java/com/api/stuv/domain/socket/service/ChatMessageService.java
+++ src/main/java/com/api/stuv/domain/socket/service/ChatMessageService.java
@@ -1,5 +1,7 @@
 package com.api.stuv.domain.socket.service;
 
+import com.api.stuv.domain.image.entity.EntityType;
+import com.api.stuv.domain.image.service.S3ImageService;
 import com.api.stuv.domain.socket.dto.ChatMessageRequest;
 import com.api.stuv.domain.socket.dto.ChatMessageResponse;
 import com.api.stuv.domain.socket.dto.ReadByResponse;
@@ -7,6 +9,7 @@ import com.api.stuv.domain.socket.dto.UserInfo;
 import com.api.stuv.domain.socket.entity.ChatMessage;
 import com.api.stuv.domain.socket.repository.ChatMessageRepository;
 import com.api.stuv.domain.socket.repository.ChatRoomRepository;
+import com.api.stuv.domain.user.dto.ImageUrlDTO;
 import com.api.stuv.domain.user.repository.UserRepository;
 import com.api.stuv.global.exception.ErrorCode;
 import com.api.stuv.global.exception.NotFoundException;
@@ -26,6 +29,7 @@ public class ChatMessageService {
     private final ChatMessageRepository chatMessageRepository;
     private final ChatRoomRepository chatRoomRepository;
     private final UserRepository userRepository;
+    private final S3ImageService s3ImageService;
     private final ChatRoomMemberService chatRoomMemberService; // 총 멤버 정보를 관리하는 서비스
 
     //메세지 불러오기
@@ -41,7 +45,10 @@ public class ChatMessageService {
                     if (userInfo == null) {
                         Long senderId = chatMessage.getSenderId();
                         String senderNickname = (senderId != null) ? userRepository.findNicknameByUserId(senderId) : "";
-                        String senderProfile = (senderId != null) ? userRepository.getCostumeInfoByUserId(senderId) : "";
+
+                        ImageUrlDTO response = userRepository.getCostumeInfoByUserId(senderId);
+                        String senderProfile = (senderId != null) ? s3ImageService.generateImageFile(EntityType.COSTUME, response.entityId(), response.newFileName()) : "";
+
                         userInfo = new UserInfo(senderId, senderNickname, senderProfile);
                     }
 
