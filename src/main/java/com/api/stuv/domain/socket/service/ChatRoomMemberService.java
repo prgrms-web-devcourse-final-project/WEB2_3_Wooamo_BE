c552a24c4ffc5f7d835f12508b54a2490181c87a
DongYoon <kdu0516@gmail.com>
2025-03-08 03:47:15 +0900

    no message

--- src/main/java/com/api/stuv/domain/socket/service/ChatRoomMemberService.java
+++ src/main/java/com/api/stuv/domain/socket/service/ChatRoomMemberService.java
@@ -23,10 +23,10 @@ import java.util.stream.Collectors;
 public class ChatRoomMemberService {
 
     // 방 이름(roomId)별로 모든 멤버 ID 목록을 저장하는 -> ReadBy
-    private final ConcurrentMap<String, List<Long>> roomTotalMembersSessions = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, List<Long>> roomTotalMembersCache = new ConcurrentHashMap<>();
     // 사용자 정보 저장 -> UserInfo
-    private final ConcurrentMap<String, Set<Long>> userRoomSessions = new ConcurrentHashMap<>();
-    private final ConcurrentMap<Long, UserInfo> userSessions = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, Set<Long>> userRoomCache = new ConcurrentHashMap<>();
+    private final ConcurrentMap<Long, UserInfo> userInfoCache = new ConcurrentHashMap<>();
 
     private final ChatRoomRepository chatRoomRepository;
     private final UserRepository userRepository;
@@ -37,7 +37,7 @@ public class ChatRoomMemberService {
         // 서버 시작 시 모든 채팅방의 멤버 정보를 불러와 roomSessions 초기화
         List<ChatRoom> chatRooms = chatRoomRepository.findAll();
         for (ChatRoom chatRoom : chatRooms) {
-            roomTotalMembersSessions.put(chatRoom.getRoomId(), new ArrayList<>(chatRoom.getMembers()));
+            roomTotalMembersCache.put(chatRoom.getRoomId(), new ArrayList<>(chatRoom.getMembers()));
         }
     }
 
@@ -46,31 +46,31 @@ public class ChatRoomMemberService {
         ChatRoom chatRoom = chatRoomRepository.findByRoomId(roomId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.CHAT_ROOM_NOT_FOUND));
         List<Long> members = chatRoom.getMembers();
-        roomTotalMembersSessions.put(roomId, new ArrayList<>(members));
+        roomTotalMembersCache.put(roomId, new ArrayList<>(members));
     }
     // 읽음 처리 계산
     public int getRoomMemberCount(String roomId) {
-        List<Long> members = roomTotalMembersSessions.get(roomId);
+        List<Long> members = roomTotalMembersCache.get(roomId);
         return (members != null) ? members.size() : 0;
     }
   // 특정 사용자가 현재 참여 중인 방 개수 반환
     public int getUserActiveRoomCount(Long userId) {
-        return (int) roomTotalMembersSessions.values().stream()
+        return (int) roomTotalMembersCache.values().stream()
                 .filter(members -> members.contains(userId))
                 .count();
     }
     // 특정 채팅방의 사용자 목록 반환
     public List<Long> getRoomMembers(String roomId) {
-        return roomTotalMembersSessions.getOrDefault(roomId, List.of());
+        return roomTotalMembersCache.getOrDefault(roomId, List.of());
     }
 
     // 사용자가 방에 입장하면 userSessions에 사용자 정보를 추가
     public void userJoinRoom(Long userId, String roomId) {
-        userRoomSessions.computeIfAbsent(roomId, k -> ConcurrentHashMap.newKeySet()).add(userId);
+        userRoomCache.computeIfAbsent(roomId, k -> ConcurrentHashMap.newKeySet()).add(userId);
 
         // 방의 모든 사용자 정보 추가
-        userRoomSessions.get(roomId).forEach(memberId -> {
-            userSessions.computeIfAbsent(memberId, id -> {
+        userRoomCache.get(roomId).forEach(memberId -> {
+            userInfoCache.computeIfAbsent(memberId, id -> {
                 String nickname = (id != null) ? userRepository.findNicknameByUserId(id) : "";
 
                 ImageUrlDTO response = (id != null) ? userRepository.getCostumeInfoByUserId(id) : null;
@@ -83,34 +83,34 @@ public class ChatRoomMemberService {
     }
     // 사용자가 특정 방에서 나가면 userSessions에서 제거
     public void userLeaveRoom(Long userId, String roomId) {
-        Set<Long> members = userRoomSessions.get(roomId);
+        Set<Long> members = userRoomCache.get(roomId);
         if (members != null) {
             members.remove(userId);
             if (members.isEmpty()) {
-                userRoomSessions.remove(roomId);
+                userRoomCache.remove(roomId);
             }
         }
 
         // 사용자가 아직 다른 방에 남아 있는지 확인 후 제거
-        boolean isUserStillInAnyRoom = userRoomSessions.values().stream()
+        boolean isUserStillInAnyRoom = userRoomCache.values().stream()
                 .anyMatch(memberSet -> memberSet.contains(userId));
 
         if (!isUserStillInAnyRoom) {
-            userSessions.remove(userId);
+            userInfoCache.remove(userId);
         }
 
     }
     // 사용자 정보 반환
     public UserInfo getUserInfo(Long userId) {
-        return userSessions.getOrDefault(userId, null);
+        return userInfoCache.getOrDefault(userId, null);
     }
 
     // 특정 채팅방의 사용자 목록 반환
     public List<UserInfo> getRoomMemberInfos(String roomId) {
-        Set<Long> userIds = userRoomSessions.getOrDefault(roomId, Collections.emptySet());
+        Set<Long> userIds = userRoomCache.getOrDefault(roomId, Collections.emptySet());
 
         return userIds.stream()
-                .map(userSessions::get)
+                .map(userInfoCache::get)
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
