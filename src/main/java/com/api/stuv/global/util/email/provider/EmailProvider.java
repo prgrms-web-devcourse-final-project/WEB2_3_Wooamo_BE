a2d8a7cb2627e205a75a7c5f8ddba73da341682a
DevPumpkin0113 <agg0113@gmail.com>
2025-03-10 16:54:59 +0900

    feat: 이메일 확인 코드 템플릿 디자인

--- src/main/java/com/api/stuv/global/util/email/provider/EmailProvider.java
+++ src/main/java/com/api/stuv/global/util/email/provider/EmailProvider.java
@@ -1,56 +1,51 @@
 package com.api.stuv.global.util.email.provider;
 
 import com.api.stuv.global.service.RedisService;
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.time.Duration;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class EmailProvider {
 
     private final JavaMailSender mailSender;
-    private final String SUBJECT ="[STUV] 인증메일입니다.";
-    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
     private final RedisService redisService;
 
-    public boolean sendMail(String email, String verificationCode) {
+    public void sendMail(String email, String verificationCode) {
         try{
             redisService.save(email, verificationCode, Duration.ofMinutes(10));
 
             MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-            String htmlContent = getCertificationMessage(verificationCode);
+            Context context = new Context();
+            context.setVariable("code", verificationCode);
+            context.setVariable("logoCid", "stuvLogo");
+            String htmlContent = templateEngine.process("email-template", context);
+
+            String subject = "[STUV] 인증메일입니다.";
 
             helper.setTo(email);
-            helper.setSubject(SUBJECT);
+            helper.setSubject(subject);
             helper.setText(htmlContent, true);
+            ClassPathResource logo = new ClassPathResource("static/STUV.png");
+            helper.addInline("stuvLogo", logo);
 
-            javaMailSender.send(message);
+            mailSender.send(message);
 
         } catch (Exception exception) {
-            exception.printStackTrace();
-            return false;
+            log.error(exception.getMessage());
         }
-
-        return true;
     }
-
-    private String getCertificationMessage(String verificationCode) {
-        String certificationMessage = "";
-
-        certificationMessage += "<h1 style='text-align: center;'>[STUV] 인증메일 </h1>";
-        certificationMessage += "<h2 style='text-align: center;'>인증 코드</h2>";
-        certificationMessage += "<h3 style='text-align: center;'>" + verificationCode + "</h3>";
-        certificationMessage += "<h3 style='text-align: center;'> 인증코드를 입력해주세요 </h3>";
-        return certificationMessage;
-
-    }
-
 }
