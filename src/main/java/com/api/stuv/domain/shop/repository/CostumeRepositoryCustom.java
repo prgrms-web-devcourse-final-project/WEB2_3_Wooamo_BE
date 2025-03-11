5d8e1e4f3977ca0c033fcf8cf1ba13a4a60bebac
you <20211939@edu.hanbat.ac.kr>
2025-03-11 15:24:21 +0900

    fix: costume 목록 조회 페이지네이션 제거

--- src/main/java/com/api/stuv/domain/shop/repository/CostumeRepositoryCustom.java
+++ src/main/java/com/api/stuv/domain/shop/repository/CostumeRepositoryCustom.java
@@ -2,13 +2,12 @@ package com.api.stuv.domain.shop.repository;
 
 import com.api.stuv.domain.image.dto.ImageDTO;
 import com.api.stuv.domain.shop.dto.costume.CostumeDTO;
-import com.api.stuv.global.response.PageResponse;
-import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CostumeRepositoryCustom {
     ImageDTO findCostumeByBestSales();
-    PageResponse<CostumeDTO> getCostumeList(Pageable pageable);
+    List<CostumeDTO> getCostumeList();
     Optional<CostumeDTO> getCostume(Long id);
 }
